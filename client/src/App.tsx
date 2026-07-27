137adf7c8534d5bbbead7d40d276036bec46e908
DayneTes <dayne3672@gmail.com>
2026-07-27 17:21:13 -0400

    Put fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Note from "./components/note";
-const API_URL = import.meta.env.API_URL
+const API_URL = import.meta.env.VITE_API_URL
 
 interface NoteData {
     id: string;
@@ -57,7 +57,7 @@ function App() {
     window.clearTimeout(saveTimers.current[safeNote.id]);
 
     saveTimers.current[safeNote.id] = window.setTimeout(() => {
-      fetch(`${API_URL}/${safeNote.id}`, {
+      fetch(`${API_URL}/api/notes${safeNote.id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
